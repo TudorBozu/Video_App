8b7a9b879b1005e1f3f2595596741fbf10be551c
Bozu Tudor <bozutudor6@gmail.com>
2024-02-13 22:11:22 +0200

    I try to fix fullscreen

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,37 +2,64 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-    // Constant for saving/restoring the video position
     private static final String VIDEO_POSITION = "video_position";
 
     private VideoView mVideoView;
     private int mVideoPosition = 0;
 
+    Boolean isPlay = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button buttonPlayVideo = findViewById(R.id.buton);
-        mVideoView = findViewById(R.id.view);
-        getWindow().setFormat(PixelFormat.UNKNOWN);
+        Button buttonrepaly = findViewById(R.id.buttonreplay);
+        mVideoView = findViewById(R.id.video_view);
 
         // Set the URI path of the video
         String uriPath = "android.resource://" + getPackageName() + "/" + R.raw.ed;
         Uri uri = Uri.parse(uriPath);
-        mVideoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        mVideoView.setMediaController(mediaController);
+        mediaController.setAnchorView(mVideoView);
 
+        mVideoView.setVideoURI(uri);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         buttonPlayVideo.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                mVideoView.start();
+                if (isPlay) {
+                    isPlay = false;
+                    mVideoView.pause();
+                } else {
+                    isPlay = true;
+                    mVideoView.start();
+                }
+
+            }
+        });
+
+        buttonrepaly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView.resume();
             }
         });
         if (savedInstanceState != null) {
